90219c4c7b71a9961bb0c65f2bbc09075ad54977
avanishpython46 <73603525+avanishpython46@users.noreply.github.com>
2020-11-05 23:31:20 +0530

    Create main2.py

---
+++ main2.py
@@ -0,0 +1,13 @@
+class Prime_generator:
+  def __init__(self,stop=None):
+    self.stop = stop
+  def __next__(self,bound):
+    for n in range(2,bound):
+        for x in range(2, n):
+          if n % x == 0:  # if n is divisible by x, it means it's not a prime number.
+            print(f"{n} equals {x} * {n//x}")
+            break
+        else:  # if n was not divisible by any x, it means it is a prime number.
+             print(f"{n} is a prime number.")
+
+l = Prime_generator()
