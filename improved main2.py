4829ff0af683ad87f08759a753e6ebe6faf753d4
avanishpython46 <73603525+avanishpython46@users.noreply.github.com>
2020-11-06 11:25:59 +0530

    Create improved main2.py

---
+++ improved main2.py
@@ -0,0 +1,19 @@
+# Define a PrimeGenerator class
+class PrimeGenerator:
+    def __init__(self, stop=None):
+        self.stop = stop
+    def Prime_generator(self,bound):
+      for n in range(2,bound):
+        for x in range(2,n):
+          if n % x==0:
+              print(f"is  not a prime number")
+          
+          else:
+            yield (f"{n} is a prime number")
+            
+          if n>=bound:
+            self.stop = StopIteration()
+            raise self.stop
+
+l = PrimeGenerator()
+pr = l.Prime_generator(12)
